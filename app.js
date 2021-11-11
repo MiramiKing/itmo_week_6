76cb3ce8ff1cbb2b5e6f175cf5e8ae6ae7e8d33d
Mikhail Ivanov <mishaivanov99@gmail.com>
2021-11-11 07:35:00 +0300

    fix

--- app.js
+++ app.js
@@ -10,35 +10,31 @@ const headers = {
 export default function initApp(express, bodyParser, fs, crypto, http) {
     const app = express();
     app
-        .use(bodyParser.urlencoded({extended: true}))
-        //.use(cors())
+        .use(bodyParser.urlencoded({extended:true}))
         .all('/login/', r => {
-            r.res.set(headers).send(login);
+            r.res.set(headers).send(login)
         })
         .all('/code/', r => {
             r.res.set(headers)
-            fs.readFile(import.meta.url.substring(7), (err, data) => {
-                if (err) throw err;
-                r.res.end(data);
-            });
+            fs.readFile(import.meta.url.substring(7),(err, data) => {
+                if (err) throw err
+                r.res.end(data)
+            })
         })
         .all('/sha1/:input/', r => {
             r.res.set(headers).send(crypto.createHash('sha1').update(r.params.input).digest('hex'))
         })
-        .get('/req/', (req, res) => {
-            let data = '';
+        .get('/req/', (req, res) =>{
             res.set(headers)
-            http.get(req.query.addr, async function (response) {
-                await response.on('data', function (chunk) {
-                    data += chunk;
-
-                }).on('end', () => {
-                })
+            let data = ''
+            http.get(req.query.addr, async function(response) {
+                await response.on('data',function (chunk){
+                    data+=chunk
+                }).on('end',()=>{})
                 res.send(data)
-
             })
         })
-        .post('/req/', r => {
+        .post('/req/', r =>{
             r.res.set(headers)
             const {addr} = req.body
             r.res.send(addr)
