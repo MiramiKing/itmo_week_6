e16b927772d591e3246d0d75d00c270a3000a55a
Mikhail Ivanov <mishaivanov99@gmail.com>
2021-11-18 16:00:23 +0300

    fix

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-export default (express, bodyParser, createReadStream, crypto, http) => {
+import {writeFileSync} from "file-system";
+
+export default (express, bodyParser, createReadStream, crypto, http, User, m, puppeteer) => {
 
     const author = 'itmo337560';
 
@@ -10,7 +12,7 @@ export default (express, bodyParser, createReadStream, crypto, http) => {
 
     const app = express();
 
-    const parseUrlEncodedBody = bodyParser.urlencoded({ extended: false })
+    const parseUrlEncodedBody = bodyParser.urlencoded({extended: false})
     app.use(parseUrlEncodedBody)
 
     app
@@ -31,15 +33,15 @@ export default (express, bodyParser, createReadStream, crypto, http) => {
         .get('/code', (req, res) => {
             let filePath = import.meta.url.replace(/^file:\/+/, '')
 
-            if (! filePath.includes(':')) {
+            if (!filePath.includes(':')) {
                 filePath = `/${filePath}`
             }
 
             createReadStream(filePath).pipe(res)
         })
 
-        .get('/sha1/:input', ({ params }, res) => {
-            const { input } = params
+        .get('/sha1/:input', ({params}, res) => {
+            const {input} = params
 
             const hash = crypto.createHash('sha1').update(input).digest('hex')
 
@@ -47,15 +49,17 @@ export default (express, bodyParser, createReadStream, crypto, http) => {
         })
 
 
-        .get('/req/', ({ query }, res) => {
-            const { addr } = query
+        .get('/req/', ({query}, res) => {
+            const {addr} = query
 
             http.get(addr, httpRes => {
                 httpRes.setEncoding('utf8')
 
                 let data = ''
 
-                httpRes.on('data', chunk => { data += chunk })
+                httpRes.on('data', chunk => {
+                    data += chunk
+                })
 
                 httpRes.on('end', () => {
                     res.send(data)
@@ -63,20 +67,103 @@ export default (express, bodyParser, createReadStream, crypto, http) => {
             })
         })
 
-        .post('/req/', ({ body }, res) => {
-            const { addr } = body
+        .post('/req/', ({body}, res) => {
+            const {addr} = body
 
             http.get(addr, httpRes => {
                 httpRes.setEncoding('utf8')
 
                 let data = ''
 
-                httpRes.on('data', chunk => { data += chunk })
-
-                httpRes.on('end', () => {
+                httpRes.on('data', chunk => {
+                    data += chunk
+                }).on('end', () => {
                     res.send(data)
                 })
             })
+        })
+
+        .post('/insert/', async (req, res) => {
+            const {login, password, URL} = req.body
+            let newUser = new User({login, password})
+            try {
+                await m.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true})
+                try {
+                    await newUser.save()
+                    res.status(201).json({'Added: ': login})
+                } catch (e) {
+                    res.status(400).json({'Error: ': 'No password entered'})
+                }
+            } catch (e) {
+                console.log(e.codeName)
+            }
+        })
+
+        .get('/wordpress/wp-json/wp/v2/posts/1', (req, res) => {
+            res.json({
+                id: 1,
+                title: {rendered: 'itmo337560'}
+            })
+        })
+
+        .post('/render/', (req, res) => {
+            const {random2, random3} = req.body
+
+            http.get(req.query.addr, {
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,OPTIONS,DELETE'
+                }
+            }, (resFrom) => {
+                const {statusCode} = resFrom;
+                let error;
+
+                if (statusCode !== 200) {
+                    error = new Error('Request Failed.\n' +
+                        `Status Code: ${statusCode}`);
+                    resFrom.resume();
+                    return;
+                }
+
+                resFrom.setEncoding('utf8');
+                let rawData = '';
+                resFrom.on('data', (chunk) => {
+                    rawData += chunk;
+                });
+                resFrom.on('end', () => {
+                    try {
+                        writeFileSync('views/template.pug', rawData, function (err) {
+                            if (err) throw err;
+                            console.log('Saved!');
+                        });
+                        res.render('template.pug', {random2, random3})
+                    } catch (e) {
+                        res.status(500)
+                    }
+                });
+            }).on('error', (e) => {
+                res.status(500)
+            }).end();
+        })
+
+        .all('/test/', async (req, res) => {
+            try {
+                let {URL} = req.query
+                const browser = await puppeteer.launch({
+                    headless: true,
+                    args: ['--no-sandbox', '--disable-setuid-sandbox']
+                })
+                let page = await browser.newPage()
+                await page.goto(URL)
+                await page.waitForSelector('#inp')
+                await page.click('#bt')
+                let got = await page.$eval('#inp', el => el.value)
+                await browser.close()
+                res.send(got)
+
+            } catch (e) {
+                console.log(e.message)
+            }
         })
 
 
--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser'
 import crypto from 'crypto';
 import http from 'http';
-import { createReadStream } from 'fs';
-
+import m from 'mongoose'
+import {createReadStream} from 'fs';
+import UserModel from './models/user.js'
+import puppeteer from 'puppeteer'
 
 import appSrc from './app.js';
 
-const app = appSrc(express, bodyParser, createReadStream, crypto, http);
-
-app.listen(process.env.PORT ?? 4321);
+const User = UserModel(m)
+const app = appSrc(express, bodyParser, createReadStream, crypto, http, User, m, puppeteer);
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`))
